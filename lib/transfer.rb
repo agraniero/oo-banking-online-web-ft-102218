c84fa0fcbf9bde08a2eb134cac61522f53cf8bc1
Anthony Graniero <ananfro@gmail.com>
2018-11-10 18:48:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,6 +12,25 @@ class Transfer
   def valid?
    self.sender.valid? && self.receiver.valid? ? true : false
   end
-  
+    
+    
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
   
 end
